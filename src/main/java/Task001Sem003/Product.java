e7e6f6b7065c79fba3471c9d81f110d26c4df2f4
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2023-04-28 14:33:14 +0300

    finish Task001

--- src/main/java/Task001Sem003/Product.java
+++ src/main/java/Task001Sem003/Product.java
@@ -4,4 +4,19 @@ public class Product {
     String nameProdukt;
     Integer price;
     String variety;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "nameProdukt='" + nameProdukt + '\'' +
+                ", price=" + price +
+                ", variety='" + variety + '\'' +
+                '}';
+    }
+
+    public Product(String nameProdukt, Integer price, String variety) {
+        this.nameProdukt = nameProdukt;
+        this.price = price;
+        this.variety = variety;
+    }
 }
--- src/main/java/Task001Sem003/Task001.java
+++ src/main/java/Task001Sem003/Task001.java
@@ -1,5 +1,8 @@
 package Task001Sem003;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task001 {
 
     /**
@@ -11,6 +14,33 @@ public class Task001 {
      * @param args
      */
     public static void main(String[] args) {
-        Product pr = new Product();
+        Product pr = new Product("мука", 69, "высший");
+        Product pr02 = new Product("бобы высший", 78, "1 сорт");
+        Product pr03 = new Product("греча высший", 59, "1 сорт");
+        Product pr04 = new Product("рис высший", 57, "2 сорт");
+        List<Product> productList = new ArrayList<>();
+        productList.add(pr);
+        productList.add(pr02);
+        productList.add(pr03);
+        productList.add(pr04);
+        int priceProductFirstVariety = 0;
+        int priceProductSecondVariety = 0;
+        System.out.println(productList);
+        for (int i = 0; i < productList.size(); i++) {
+            if (productList.get(i).variety.equals("1 сорт") && productList.get(i).nameProdukt.contains("высший")) {
+                if (productList.get(i).price > priceProductFirstVariety) {
+                    priceProductFirstVariety = productList.get(i).price;
+                }
+            }
+            if (productList.get(i).variety.equals("2 сорт") && productList.get(i).nameProdukt.contains("высший")) {
+                if (productList.get(i).price > priceProductSecondVariety) {
+                    priceProductSecondVariety = productList.get(i).price;
+                }
+            }
+
+        }
+        System.out.println("priceProductFirstVariety = " + priceProductFirstVariety);
+        System.out.println("priceProductSecondVariety = " + priceProductSecondVariety);
     }
+
 }

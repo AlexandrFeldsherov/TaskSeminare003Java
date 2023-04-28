19c3f7e2d1ddac986f3ee87f1acca64502027cf0
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2023-04-28 12:59:58 +0300

    class product

--- src/main/java/Task001Sem003/Product.java
+++ src/main/java/Task001Sem003/Product.java
@@ -1,5 +1,7 @@
 package Task001Sem003;
 
 public class Product {
-
+    String nameProdukt;
+    Integer price;
+    String variety;
 }
--- src/main/java/Task001Sem003/Task001.java
+++ src/main/java/Task001Sem003/Task001.java
@@ -1,6 +1,15 @@
 package Task001Sem003;
 
 public class Task001 {
+
+    /**
+     * Дан массив записей:
+     * наименование товара,
+     * цена, сорт.
+     * Найти наибольшую цену товаров 1го или 2го сорта среди товаров, название которых содержит «высший».
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Product pr = new Product();
     }
